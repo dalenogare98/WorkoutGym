8df41991e12c3080b20e9a7bec3382803b766973
pedro.dalenogare.glo <pedro.dalenogare@globalhitss.com.br>
2025-10-17 16:44:42 -0300

    mais ajustes

--- src/app/components/view/bolao/bolao.component.ts
+++ src/app/components/view/bolao/bolao.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-bolao',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bolao.component.css']
 })
 export class BolaoComponent implements OnInit {
+  title: string = '';
+  links: { label: string; url: string }[] = [];
+  imageSrc: string = '';
+  imageAlt: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('assets/bolao.json').subscribe((data) => {
+      this.title = data?.title ?? '';
+      this.links = data?.links ?? [];
+      this.imageSrc = data?.imageSrc ?? '';
+      this.imageAlt = data?.imageAlt ?? '';
+    });
   }
-
 }
