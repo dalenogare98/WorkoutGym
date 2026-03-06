a24f3dfd28ec8ff2f76a5c020ca903e49e73b416
pedro.dalenogare.glo <pedro.dalenogare@globalhitss.com.br>
2026-03-05 22:28:03 -0300

    conflict

--- src/app/components/content/content4/content4.component.ts
+++ src/app/components/content/content4/content4.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content4.component.css']
 })
 export class Content4Component implements OnInit {
+  plans: any[] = [];
+  disclaimers: string[] = [];
+  pricesPdfPath: string = 'assets/tabela_de_precos_2026.pdf';
 
   constructor() { }
 
